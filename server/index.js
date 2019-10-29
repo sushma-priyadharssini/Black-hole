6ec056f622593ad5bf1b7388db508b7f2c9274b2
Sushma Priyadharssini KV <kvsushmapriyadharssini@gmail.com>
2019-10-29 12:21:12 +0530

    socket calls

---
+++ server/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+let rooms = 0;
+
+app.use(express.static('.'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'game.html'));
+});
+
+server = app.listen(4000);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('connected!');
+    // Create a new game room and notify the creator of game.
+    socket.on('createGame', (data) => {
+        console.log('lala');
+        socket.join(`room-${++rooms}`);
+        console.log(`room-${rooms}`);
+        socket.emit('newGame', { name: data.name, room: `room-${rooms}` });
+    });
+
+    // Connect the Player 2 to the room he requested. Show error if room full.
+    socket.on('joinGame', function (data) {
+        var room = io.nsps['/'].adapter.rooms[data.room];
+        if (room && room.length === 1) {
+            socket.join(data.room);
+            socket.broadcast.to(data.room).emit('player1', {});
+            socket.emit('player2', { name: data.name, room: data.room })
+        } else {
+            socket.emit('err', { message: 'Sorry, The room is full!' });
+        }
+    });
+
+    /**
+       * Handle the turn played by either player and notify the other.
+       */
+    socket.on('playTurn', (data) => {
+        socket.broadcast.to(data.room).emit('turnPlayed', {
+            tile: data.tile,
+            room: data.room
+        });
+    });
+
+    /**
+       * Notify the players about the victor.
+       */
+    socket.on('gameEnded', (data) => {
+        socket.broadcast.to(data.room).emit('gameEnd', data);
+    });
+});
